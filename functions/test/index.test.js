c1bba4745258da89da0a622b501c69344c84878a
parkernewland <pjnewland21@gmail.com>
2020-05-11 13:58:34 -0400

    added test

---
+++ functions/test/index.test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+const index = require('../index');
+describe('index', function() {
+    it('Converts Kelvin to Fahrenheit', function () {
+        let result = index.KtoF(273.15);
+        assert.equal(result, 32)
+    })
+});
